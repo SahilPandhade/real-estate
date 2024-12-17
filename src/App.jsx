bacebd205ad9046a8ce6f10c8d3abd1855045162
SahilPandhade <sahilpandhade@rediffmail.com>
2024-12-17 23:48:58 +0530

    C-04:Added List View and HomePage Search Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import './layout.scss'
-import Navbar from './components/navbar/Navbar'
-import HomePage from './routes/homePage/HomePage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './routes/layout/Layout';
+import HomePage from './routes/homePage/HomePage';
+import ListPage from './routes/listPage/ListPage';
+import SinglePage from './routes/SinglePage/SinglePage';
 function App() {
+  const router  = createBrowserRouter([
+    {
+      path:'/',
+      element:(
+       <Layout/>
+      ),
+      children:[
+        {
+          path:'/',
+          element:<HomePage/>
+        },
+        {
+          path:'/list',
+          element:<ListPage/>
+        },
+        {
+          path:'/:id',
+          element:<SinglePage/>
+        },
+      ]
+
+    }
+  ]);
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar/>
-      </div>
-      <div className="content">
-        <HomePage/>
-      </div>
-      
-    </div>
+
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/routes/listPage/ListPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './listPage.scss'
+import { listData } from '../../lib/DummyData'
+import Filter from '../../components/filter/Filter'
+import Card from '../../components/Card/Card'
+const ListPage = () => {
+  const data = listData
+  return (
+    <div className='listPage'>
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item)=>(
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">
+        Map
+      </div>
+    </div>
+  )
+}
+
+export default ListPage
