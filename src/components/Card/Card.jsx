7713393b0d2b76ac6976e2ae5c6bcbdea5f003b9
SahilPandhade <sahilpandhade@rediffmail.com>
2024-12-18 22:45:05 +0530

    C-05: Added Card Component on List Page

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
-
-const Card = () => {
+import { Link } from 'react-router-dom'
+import './Card.scss'
+const Card = ({ item }) => {
   return (
-    <div>Card</div>
+    <div className='card'>
+      <Link to={`/${item.id}`} className='imageContainer'>
+        <img src={item.img} alt="" />
+      </Link>
+      <div className="textContainer">
+        <h2 className='title'>
+          <Link to={`/${item.id}`}>{item.title}</Link>
+        </h2>
+        <p className='address'>
+          <img src='/pin.png' alt="" />
+          <span>{item.address}</span>
+        </p>
+        <p className='price'>
+          ${item.price}
+        </p>
+        <div className="bottom">
+          <div className="features">
+            <div className="feature">
+              <img src='/bed.png' alt=""></img>
+              <span>{item.bedroom} bedrooms</span>
+            </div>
+            <div className="feature">
+              <img src='/bath.png' alt=""></img>
+              <span>{item.bathroom} bathrooms</span>
+            </div>
+          </div>
+          <div className='icons'>
+            <div className='icon'>
+              <img src='/save.png' alt="" />
+
+            </div>
+            <div className='icon'>
+              <img src='/chat.png' alt="" />
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
