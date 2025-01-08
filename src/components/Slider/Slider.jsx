2804a401ef4b6314ccfe320876d14f64e8255d52
SahilPandhade <sahilpandhade@rediffmail.com>
2025-01-08 23:11:29 +0530

    C-07: Added Slider Component for Property Images

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import {React,useState} from 'react'
 import './Slider.scss'
-const Slider = () => {
+
+const Slider = ({ images }) => {
+  const [imageIndex, setImageIndex] = useState(null);
   return (
-    <div>Slider</div>
+    <div className='slider'>
+      {imageIndex!==null &&
+      (<div className="fullSlider">
+        <div className="arrow">
+          <img src='./arrow.png' alt=''  onClick={()=>setImageIndex(imageIndex==0 ? images.length-1 :imageIndex-1)}/>
+        </div>
+        <div className="imgContainer">
+          <img src={images[imageIndex]} alt="" />
+        </div>
+        <div className="arrow">
+          <img src='./arrow.png' className='right' alt='' onClick={()=>setImageIndex((imageIndex+1)%images.length)}/>
+        </div>
+        <div className="close" onClick={()=>setImageIndex(null)}>
+          X
+        </div>
+      </div>
+      )}
+      <div className="bigImage">
+        <img src={images[0]} alt=""  onClick={()=>setImageIndex(0)}/>
+      </div>
+      <div className="smallImage">
+        {
+          images.slice(1).map((image, index) => (
+            <img src={image} alt="" key={index} onClick={()=>setImageIndex(index+1)}/>
+          ))
+        }
+
+      </div>
+      
+
+    </div>
   )
 }
 
