82122296d0c1e52c4efddf1a1d910b74c8123b48
SahilPandhade <sahilpandhade@rediffmail.com>
2025-01-17 00:11:19 +0530

    C-07:Added Property Feature Page and Responsive Design

--- src/routes/SinglePage/SinglePage.jsx
+++ src/routes/SinglePage/SinglePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Slider from '../../components/Slider/Slider'
 import { singlePostData, userData } from '../../lib/DummyData'
-
+import Map from '../../components/Map/Map.jsx'
 import './SinglePage.scss'
 const SinglePage = () => {
   return (
@@ -15,24 +15,111 @@ const SinglePage = () => {
               <div className="post">
                 <h1>{singlePostData.title}</h1>
                 <div className="address">
-                  <img src='./pin.png' alt=""/>
+                  <img src='./pin.png' alt="" />
                   <span>{singlePostData.address}</span>
                 </div>
                 <div className="price">$ {singlePostData.price}</div>
               </div>
               <div className="user">
-                  <img src={userData.img} alt=""/>
-                  <span>{userData.name}</span>
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
               </div>
             </div>
             <div className="bottom">
-                {singlePostData.description}
+              {singlePostData.description}
             </div>
           </div>
         </div>
       </div>
       <div className="features">
-        <div className="wrapper"></div>
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+
+            <div className="feature">
+              <img src="/utility.png" alt="" />
+              <div className="featureText">
+                <span>Utilities</span>
+                <p>Renter is Responsible</p>
+              </div>
+            </div>
+
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Pet Policy</span>
+                <p>Pets Allowed</p>
+              </div>
+            </div>
+
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Property Fees</span>
+                <p>Must have 3x the rent in total household income</p>
+              </div>
+            </div>
+
+          </div>
+          <p className="title">Sizes</p>
+          <div className="sizes">
+            <div className="size">
+              <img src="/size.png" alt="" />
+              <span>80 sqft</span>
+            </div>
+            <div className="size">
+              <img src="/bed.png" alt="" />
+              <span>2 Beds</span>
+            </div>
+            <div className="size">
+              <img src="/bath.png" alt="" />
+              <span>1 Bathroom</span>
+            </div>
+
+          </div>
+          <p className="title">Nearby Places</p>
+          <div className="listHorizontal">
+            <div className="feature">
+              <img src="/school.png" alt="" />
+              <div className="featureText">
+                <span>School</span>
+                <p>250m Away</p>
+              </div>
+
+            </div>
+            <div className="feature">
+              <img src="/pet.png" alt="" />
+              <div className="featureText">
+                <span>Bus Stop</span>
+                <p>100m Away</p>
+              </div>
+
+            </div>
+            <div className="feature">
+              <img src="/fee.png" alt="" />
+              <div className="featureText">
+                <span>Restaurant</span>
+                <p>200m Away</p>
+              </div>
+
+            </div>
+          </div>
+          <p className="title">Location</p>
+          <div className="mapContainer">
+            <Map items={[singlePostData]} />
+          </div>
+
+          <div className="buttons">
+            <button>
+              <img src='chat.png' alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src='save.png' alt="" />
+              Save the Place
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   )
